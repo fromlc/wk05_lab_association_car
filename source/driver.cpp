f459e65219f340e65e7c3fa3fc567d0e0a6b6507
L Carver <wifihum@gmail.com>
2023-02-27 19:25:50 -0800

    add function inputLoop()

--- source/driver.cpp
+++ source/driver.cpp
@@ -19,9 +19,10 @@ constexpr int GASMAX = 6;
 constexpr int ENGINESLEFT = 3;
 
 //------------------------------------------------------------------------------
-// prototypes
+// local function prototypes
 //------------------------------------------------------------------------------
 void displayBanner();
+void inputLoop();
 
 //------------------------------------------------------------------------------
 // entry point
@@ -30,45 +31,7 @@ int main() {
 
     displayBanner();
 
-    // app setup
-    Car gto;
-    int enginesLeft = ENGINESLEFT;
-
-    // input loop
-    while (enginesLeft) {
-
-        cout << "How much gas? ";
-        int gasAmount;
-        cin >> gasAmount;
-
-        // quit on 0 gas
-        if (!gasAmount) {
-            break;
-        }
-
-        // hit the gas
-        gasAmount = abs(gasAmount);
-        if (gasAmount > GASMAX) {
-            cout << "GTO engine blown! Engines left: " 
-                << --enginesLeft << "\n\n";
-            continue;
-        }
-
-        // gas pedal controls engine rpms and transmission gear
-        gto.setGasPedal(gasAmount);
-
-        cout << "Gas pedal: " << gto.getGasPedal() << '\n';
-        cout << "Engine RPMs: " << gto.getRpms() << '\n';
-
-        int gear = gto.getGear() + 1;
-        cout << "Transmission gear: " << gear << '\n';
-
-        cout << "Vroom";        
-        for (int i = 0; i < gear; ++i) {
-            cout << '!';
-        }
-        cout << "\n\n";
-    }
+    inputLoop();
 
     cout << "Goodbye!\n\n";
 
@@ -83,4 +46,48 @@ void displayBanner() {
     cout << "\nDrive a GTO!\n";
     cout << "Enter gas amounts from 1 to " << GASMAX << ", 0 quits.\n";
     cout << "You can blow up to " << ENGINESLEFT << " engines.\n\n";
+}
+
+//------------------------------------------------------------------------------
+// process user commands
+//------------------------------------------------------------------------------
+void inputLoop() {
+
+    Car gto;
+    int enginesLeft = ENGINESLEFT;
+
+    while (enginesLeft) {
+
+        cout << "How much gas? ";
+        int gasAmount;
+        cin >> gasAmount;
+
+        // quit on 0 gas
+        if (!gasAmount) {
+            break;
+        }
+
+        // hit the gas
+        gasAmount = abs(gasAmount);
+        if (gasAmount > GASMAX) {
+            cout << "GTO engine blown! Engines left: "
+                << --enginesLeft << "\n\n";
+            continue;
+        }
+
+        // gas pedal controls engine rpms and transmission gear
+        gto.setGasPedal(gasAmount);
+
+        cout << "Gas pedal: " << gto.getGasPedal() << '\n';
+        cout << "Engine RPMs: " << gto.getRpms() << '\n';
+
+        int gear = gto.getGear() + 1;
+        cout << "Transmission gear: " << gear << '\n';
+
+        cout << "Vroom";
+        for (int i = 0; i < gear; ++i) {
+            cout << '!';
+        }
+        cout << "\n\n";
+    }
 }
